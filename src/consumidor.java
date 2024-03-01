2b0b61ebea9a0dbf753fdac19b80c047bc5e8e49
AnthonyHaro <anthony.haro@epn.edu.ec>
2024-03-01 07:31:35 -0500

    creacion del guardado de facturas

--- src/consumidor.java
+++ src/consumidor.java
@@ -1,9 +1,7 @@
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,6 +9,9 @@ import java.sql.Statement;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import static org.apache.pdfbox.pdmodel.font.PDType1Font.*;
 
 public class consumidor {
     JPanel consumidor;
@@ -24,12 +25,12 @@ public class consumidor {
                 JFrame frame = (JFrame) SwingUtilities.getWindowAncestor(xButton);
                 frame.dispose();
                 JFrame frames = new JFrame("EL MINI MINI MARKET PRO PLUS +");
-                frame.setUndecorated(true);
-                frame.setContentPane(new Cajero().cajerop);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(600,600);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+                frames.setUndecorated(true);
+                frames.setContentPane(new Cajero().cajerop);
+                frames.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frames.setSize(600, 600);
+                frames.setLocationRelativeTo(null);
+                frames.setVisible(true);
             }
         });
         vistobut.addActionListener(new ActionListener() {
@@ -41,44 +42,30 @@ public class consumidor {
                     String usuario = "root";
                     String contraseña = "";
                     try (Connection conexion = DriverManager.getConnection(url, usuario, contraseña)) {
-
                         String consulta = "SELECT * FROM facturas_temp";
-
                         try (Statement statement = conexion.createStatement();
                              ResultSet resultSet = statement.executeQuery(consulta);
-                             PrintWriter writer = new PrintWriter(new FileWriter("datos.txt"))) {
+                             PDDocument document = new PDDocument()) {
+                            PDPage page = new PDPage();
+                            document.addPage(page);
+                            PDPageContentStream contentStream = new PDPageContentStream(document, page);
+                            contentStream.setFont(PDType1Font.HELVETICA, 12);
                             while (resultSet.next()) {
                                 String columna1 = resultSet.getString("nombre_p");
                                 String columna2 = resultSet.getString("precio");
                                 String columna3 = resultSet.getString("cantidad");
                                 String columna4 = resultSet.getString("sub_total");
-                                writer.println(columna1 + "\t" + columna2 + "\t" + columna3 + "\t" +columna4);
-                            }
-                            System.out.println("Datos guardados en datos.txt correctamente.");
-                            try (PDDocument document = new PDDocument()) {
-                                PDPage page = new PDPage();
-                                document.addPage(page);
 
-                                PDPageContentStream contentStream = new PDPageContentStream(document, page);
                                 contentStream.beginText();
                                 contentStream.newLineAtOffset(100, 700);
-                                try (BufferedReader reader = new BufferedReader(new FileReader("D:/csdavid/Proyecto-POO-Final/datos.txt"))) {
-                                    String line;
-                                    while ((line = reader.readLine()) != null) {
-                                        contentStream.showText(line);
-                                        contentStream.newLine();
-                                    }
-                                }
-
+                                contentStream.showText(columna1 + " - " + columna2 + " - " + columna3 + " - " + columna4);
                                 contentStream.endText();
-                                contentStream.close();
-
-                                document.save("datos.pdf");
-                                System.out.println("Archivo PDF creado correctamente.");
                             }
-
+                            contentStream.close();
+                            document.save("datos2.pdf");
+                            System.out.println("PDF creado correctamente.");
                         } catch (Exception ex) {
-                            System.err.println("Error al convertir el archivo: " + ex.getMessage());
+                            System.err.println("Error al manipular el PDF: " + ex.getMessage());
                         }
 
                     } catch (Exception ex) {
